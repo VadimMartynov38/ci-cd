246711d5ce16cc88cd8f4ac478579a89560f4d4d
VadimMartynov38 <120673281+VadimMartynov38@users.noreply.github.com>
2026-01-30 22:50:24 +0300

    Create schemas.py

---
+++ src/schemas.py
@@ -0,0 +1,37 @@
+from typing import List, Optional
+from pydantic import BaseModel, Field, conint
+
+class IngredientBase(BaseModel):
+    name: str
+    quantity: Optional[str]
+
+class IngredientCreate(IngredientBase):
+    pass
+
+class Ingredient(IngredientBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class RecipeBase(BaseModel):
+    title: str
+    cook_time: int
+
+class RecipeCreate(RecipeBase):
+    ingredients: Optional[List[IngredientCreate]]
+
+class RecipeListItem(RecipeBase):
+    id: int
+    views: int
+
+    class Config:
+        orm_mode = True
+
+class Recipe(RecipeBase):
+    id: int
+    views: int
+    ingredients: List[Ingredient]
+
+    class Config:
+        orm_mode = True
