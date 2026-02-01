55b8736da6a5b38199f61f94349361b7162e3a84
HP 5 <vm4824036@gamil.com>
2026-02-01 14:11:13 +0300

    update_mypy

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+db: SQLAlchemy = SQLAlchemy()
 
 
 def create_app():
